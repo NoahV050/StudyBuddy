f39c485103b7547001b44ae8c74b65d9a53d3a71
NoahV050 <ikbenbeter0508@gmail.com>
2026-04-23 21:04:06 +0200

    PWA model gemaakt voor telefoon gebruik.

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'studiemaatje-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/js/script.js',
+  '/js/config.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
